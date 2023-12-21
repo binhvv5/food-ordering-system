8deb66b6fafcb1795ded2430e187c120acd4b13f
Brian Vu <brian_vu@vn.gemteks.com>
2023-12-21 11:47:23 +0700

    add init sql for customer

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('e37b3101-0ea9-48f2-b1d2-8e49d1e292a0', 'user-1', 'First', 'User')
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('136a9692-f4ee-415d-a9f3-737637e2412e', 'user-2', 'Second', 'User')
